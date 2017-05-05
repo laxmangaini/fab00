091aeaa248d61fc7ef9fdec68e0db6f72187c401
laxmangaini <laxmangaini534@gmail.com>
2017-05-05 16:31:08 +0530

    tested

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import local
 def prepare_deploy():
-  local("cd /home/devops/python/fabric-scripts/fab00 && touch f1 f2 f3")
-  local("cd /home/devops/python/fabric-scripts/fab00 && git add . && git commit -m test")
-  local("cd /home/devops/python/fabric-scripts/fab00 && git push origin master")
+  local("cd /home/visualpath/python/fab00 && touch 1a 2a 3a")
+  local("cd /home/visualpath/python/fab00 && git add . && git commit -m tested")
+  local("cd /home/visualpath/python/fab00 && git push origin master")
 
